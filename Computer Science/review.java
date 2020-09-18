d1c1d03759199d5fa24a5376db8970bdb8bc6542
Des <40222729+notDes@users.noreply.github.com>
2020-09-18 12:43:01 -0400

    Update review.java

--- Computer Science/review.java
+++ Computer Science/review.java
@@ -16,8 +16,3 @@ public class review
 
 
 
-/*
-E1.17: Modify the program from E1.16 so that the dialog continues with the message “My name is Hal! What would you like me to do?” Discard the user’s input and display a message such as
-I'm sorry, Dave. I'm afraid I can't do that.
-Replace Dave with the name that was provided by the user.
-*/
