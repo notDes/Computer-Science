5f1396eb4e98aafcb7a5716b6ce2f6f59d70725c
Des <40222729+notDes@users.noreply.github.com>
2020-09-18 12:44:53 -0400

    Update KingsIsland.java

--- Computer Science/KingsIsland.java
+++ Computer Science/KingsIsland.java
@@ -6,8 +6,6 @@ public class KingsIsland
 	{
 		Scanner input = new Scanner(System.in);
 		
-		// Declare variables for number of people, parking cost,
-		// food cost, ticket cost, total cost, and cost per person
 		int numberOfPeople;
 		double parkingCost;
 		double foodCost;
@@ -15,36 +13,24 @@ public class KingsIsland
 		double totalCost;
 		double costPerPerson;
     
-		// Prompt for and collect input for number of people in 
-		// the group.
-		// Hint: use input.nextInt() to collect an integer number
 		System.out.print("Enter the number of people:");
 		numberOfPeople = input.nextInt();
     
-		// Prompt for and collect input for cost of parking
-		// for example, $5.50
+
 		System.out.print("Enter the cost for parking:");
 		parkingCost = input.nextDouble();
-
-
 		
 		
-		// Prompt for and collect amount spent on food
-		// for example, $15.75
 		System.out.print("Enter the cost for food:");
 		foodCost = input.nextDouble();
 		
-		
-		// Calculate the total expenses. Each ticket is $32.99
 		ticketCost = 32.99;
 
 		double totalTicketCost = ticketCost * numberOfPeople;
 
-		// System.out.print(totalTicketCost); debugging
 
 		totalCost = totalTicketCost + foodCost + parkingCost; // use this in results
 		
-		// Calculate the average cost per person
 
 
 		costPerPerson = totalCost / numberOfPeople; // use this in results
