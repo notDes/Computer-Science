5135201292513f88a10054d667646b3405c286ae
Des <Notdestroyer@gmail.com>
2020-09-25 19:56:25 -0400

    new project !

--- Computer Science/stringExercises.java
+++ Computer Science/stringExercises.java
@@ -1,29 +1,48 @@
-class stringExcerises {
-    public static void main(String[] args) {
-      System.out.println("R2.9");
-      
-      String str = "Hello";
-      
-      String helloToLowerCase = str.toLowerCase();
-  
-      System.out.println(helloToLowerCase);
+import java.lang.StringBuilder;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("R2.9");
     
-      
-      System.out.println("R2.10");
-      
-      String helloToUpperCase = str.toUpperCase();
+    String str = "Hello";
+    
+    String helloToLowerCase = str.toLowerCase();
+
+    System.out.println(helloToLowerCase);
   
-      System.out.println(helloToUpperCase);
-      
-      
-      System.out.println("E2.3");
-      
-      String strMississippi = "Mississippi";
-      
-      System.out.println("E2.8");
-      
-      
-      
-      
-    }
+    
+    System.out.println("R2.10");
+    
+    String helloToUpperCase = str.toUpperCase();
+
+    System.out.println(helloToUpperCase);
+    
+    
+    System.out.println("E2.3");
+    
+    String strMississippi = "Mississippi";
+    
+    String newMississippi = strMississippi.replace("i", "ii");
+    
+    System.out.println(newMississippi.length());
+    
+    String newMississippi2 = strMississippi.replace("ss", "s");
+    
+    System.out.println(newMississippi2.length());
+    
+    
+    System.out.println("E2.8");
+    
+    
+    // System.out.println(strDesserts.reverse().toString());
+
+    StringBuilder desserts = new StringBuilder("desserts");
+
+    System.out.println(desserts.toString());
+
+    
+    
+    
+    
   }
+}
