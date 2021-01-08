5150fe80bdf6c948e6ce4fbd05025356205e510b
Des <Notdestroyer@gmail.com>
2021-01-07 19:06:16 -0500

    for and while loops

---
+++ SecondSemester/ForLoops.java
@@ -0,0 +1,13 @@
+package SecondSemester;
+import java.util.Scanner;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Gimme a positive integer: ");
+        int posInt = input.nextInt();
+
+        System.out.print("The sum of all even numbers between 2 and " + posInt );
+    }
+
+}
--- SecondSemester/WhileLoops.java
+++ SecondSemester/WhileLoops.java
@@ -1,4 +1,4 @@
-package Chapter4;
+package SecondSemester;
 import java.util.Scanner;
 
 
@@ -9,11 +9,33 @@ public class WhileLoops {
     System.out.print("Gimme a positive integer: ");
     int posInt = input.nextInt();
     int num = 0;
+    System.out.println(" ");
 
-    while(num < posInt){
-    int square = num * num;
+    System.out.print("Squares less than " + posInt + " = ");
+
+    while (num * num < posInt) {
+      int square = num * num;
       num++;
-      System.out.print(square);
+      System.out.print(" " + square);
     }
-}
+    System.out.println(" ");
+    System.out.print("Tens less than " + posInt + " = ");
+
+    num = 1;
+    while (num * 10 < posInt) {
+      int ans = num * 10;
+      System.out.print(" " + ans);
+      num++;
+
+    }
+    System.out.println(" ");
+    System.out.print("Powers of two less than " + posInt + " = ");
+
+    num = 1;
+    while (num < posInt) {
+      System.out.print(num + " ");
+      num = num * 2;
+    }
+
+  }
 }
